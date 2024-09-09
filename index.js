96445a5ba00248ac32599c7608cbbef4302eb7a1
PhuongUyen0712 <ledoanphuonguyen0712@gmail.com>
2024-09-09 13:59:40 +0700

    feat: ađ update delete

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Đầu tiên mình sẽ để một dòng console.log để kiểm tra xem app có chạy bình thường không
-// console.log("!!!!")
+// console.log("!!!!");
 
 //Để sử dụng app express
 const express = require("express");
@@ -7,45 +7,69 @@ const app = express();
 
 app.use(express.json());
 
-//GET API
+// //GET API
 
-//Khởi tạo mảng rỗng
-const todos = [];
+app.get("/", (req, res) => {
+  return res.send("Hello World!!");
+});
 
-//Chọn phương thức GET, return về mảng todos hiện tại dưới dạng json 
+// // //Khởi tạo mảng rỗng
+// const todos = [];
 
-// Lần đầu chạy sẽ ra mảng rỗng, vì khởi tạo bên trên là mảng rỗng
-app.get("/todos", (req, res) => {
-    return res.json(todos);
-})
+// // //Chọn phương thức GET, return về mảng todos hiện tại dưới dạng json
 
-//POST API
+// // // Lần đầu chạy sẽ ra mảng rỗng, vì khởi tạo bên trên là mảng rỗng
+// app.get("/todos", (req, res) => {
+//   return res.json(todos);
+// });
 
-//Chọn phương thức POST
-app.post("/todos", (req, res) => {
-    //Lấy title từ body của request
-    //Ở đây mình cũng có thể lấy
-    //const title = req.body.title;
-    const { title } = req.body;
+// // //POST API
 
-    //Tạo object todo với title đã lấy từ body của request
-    //Để mặc định completed là false
-    const todo = {
-        title,
-        completed: false
-    }
+// //Chọn phương thức POST
+// app.post("/todos", (req, res) => {
+//   //Lấy title từ body của request
+//   //Ở đây mình cũng có thể lấy
+//   //const title = req.body.title;
+//   const { title } = req.body;
 
-    //Push phần tử todo này vào mảng todos
-    todos.push(todo);
+//   //Tạo object todo với title đã lấy từ body của request
+//   //Để mặc định completed là false
+//   const todo = {
+//     id: todos.length + 1,
+//     title,
+//     // completed: false,
+//   };
 
-    //Return về phần tử todo dưới dạng json
-    return res.json(todo)
-})
+//   //Push phần tử todo này vào mảng todos
+//   todos.push(todo);
 
+//   //Return về phần tử todo dưới dạng json
+//   return res.json(todo);
+// });
+
+// // app.put("/todos/:id", (req, res) => {
+// //   console.log(req.params);
+// //   const todo = todos.find((i) => i.id == req.params.id);
+// //   if (todo) {
+// //     todo.title = req.body.title;
+// //     res.json(todo);
+// //   } else {
+// //     res.status(404).send("Todo not found");
+// //   }
+// // });
+
+// // app.delete("/todos/:id", (req, res) => {
+// //   const todo = todos.find((i) => i.id == req.params.id);
+// //   const todoIndex = todos.indexOf(todo);
+// //   if (todoIndex !== -1) {
+// //     todos.splice(todoIndex, 1);
+// //     res.status(204).send();
+// //   } else {
+// //     res.status(404).send("Item not found");
+// //   }
+// // });
 //Run app ở port 8080
 app.listen(8080, () => {
-
-    //Kiểm tra app có đang chạy không
-    console.log("The app is running")
-})
-
+  //Kiểm tra app có đang chạy không
+  console.log("The app is running");
+});
